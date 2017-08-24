c9537a8aeb65e5044e43c5c4663271b58c575901
Richter <grichter@spokanecity.org>
2017-08-24 08:44:03 -0700

    Handled bitbucket merge and unrelatede-histories merge.

---
+++ BudgetDashboardAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BudgetDashboardAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BudgetDashboardAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BudgetDashboardAPI.WebApiApplication" Language="C#" %>
